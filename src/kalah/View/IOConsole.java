7241232abbb0b19ad232d8f34695849d19301bb3
ktam069 <ktam069@aucklanduni.ac.nz>
2019-06-07 19:18:37 +1200

    Added ASCIIBoard to represent the layout of the game

--- src/kalah/View/IOConsole.java
+++ src/kalah/View/IOConsole.java
@@ -1,7 +1,6 @@
 package kalah.View;
 
 import com.qualitascorpus.testsupport.IO;
-import kalah.Settings;
 
 import java.util.List;
 
@@ -21,60 +20,11 @@ public class IOConsole {
     }
 
     public void printGameState(List<Integer> storeSeeds, List<List<Integer>> houseSeedsList) {
-        String lineToPrint1, lineToPrint2;
+        List<String> linesToPrint = ASCIIBoard.formatBoardAsLines(storeSeeds, houseSeedsList);
 
-        lineToPrint1 = formatLine1(storeSeeds, houseSeedsList);
-        lineToPrint2 = formatLine2(storeSeeds, houseSeedsList);
-
-        io.println("+----+-------+-------+-------+-------+-------+-------+----+");
-        io.println(lineToPrint1);
-        io.println("|    |-------+-------+-------+-------+-------+-------|    |");
-        io.println(lineToPrint2);
-        io.println("+----+-------+-------+-------+-------+-------+-------+----+");
-    }
-
-    private String formatLine1(List<Integer> storeSeeds, List<List<Integer>> houseSeedsList) {
-        String lineToPrint;
-        List<Integer> houseSeeds;
-        int playerI = 0;
-
-        // Format P2 Houses
-        playerI = nextPlayerIndex(playerI);
-        lineToPrint = "| P"+(playerI+1)+" |";
-        houseSeeds = houseSeedsList.get(playerI);
-        for (int i = houseSeeds.size()-1; i >= 0 ; i--) {
-            lineToPrint += String.format("%2d[%2d] |", i+1, houseSeeds.get(i));
+        for (String line : linesToPrint) {
+            io.println(line);
         }
-
-        // Format P1 Store
-        playerI = nextPlayerIndex(playerI);
-        lineToPrint += String.format(" %2d |", storeSeeds.get(playerI));
-
-        return lineToPrint;
-    }
-
-    private String formatLine2(List<Integer> storeSeeds, List<List<Integer>> houseSeedsList) {
-        String lineToPrint;
-        List<Integer> houseSeeds;
-        int playerI = 0;
-
-        // Format P2 Store
-        playerI = nextPlayerIndex(playerI);
-        lineToPrint = String.format("| %2d |", storeSeeds.get(1));
-
-        // Format P1 Houses
-        playerI = nextPlayerIndex(playerI);
-        houseSeeds = houseSeedsList.get(playerI);
-        for (int i = 0; i < houseSeeds.size(); i++) {
-            lineToPrint += String.format("%2d[%2d] |", i+1, houseSeeds.get(i));
-        }
-        lineToPrint += " P"+(playerI+1)+" |";
-
-        return lineToPrint;
-    }
-
-    private int nextPlayerIndex(int playerI) {
-        return (playerI+1) % Settings.NUM_PLAYERS;
     }
 
     public String getUserInput(int playerNum) {
